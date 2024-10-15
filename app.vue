1c3c059b1fc8a71f65311dd20cd5b92ecd86e5b9
giacomomarongiu <giacomomarongiu1993@gmail.com>
2024-10-15 17:00:52 +0200

    exploring NuxtJS

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <nav style="display: flex; gap: 10px" >
+      <NuxtLink to="/">Homepage</NuxtLink>
+      <NuxtLink to="/movies">Movies</NuxtLink>
+      <NuxtLink to="https://vueschool.io">Vue School</NuxtLink>
+
+    </nav>
+  </div>
+    Hello Nuxt!
+    <NuxtPage></NuxtPage>
+
+</template>
+<script setup lang="ts">
+</script>
---
+++ pages/movies/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<MovieSearch />
+</template>
+
+<style scoped></style>
